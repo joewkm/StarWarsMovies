829c30d523200b2218747a9187611605b49deebe
J Wong <joewkm@gmail.com>
2022-01-23 13:00:39 -0800

    Update 1

---
+++ build/precache-manifest.be868d597ad47d41879252a3469239ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1446d5738654f66fce138063590ab8d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af30b4a860ce076108f",
+    "url": "/static/css/main.ae93a6e1.chunk.css"
+  },
+  {
+    "revision": "073b321df77737db16f0",
+    "url": "/static/js/2.ad3588c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ad3588c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af30b4a860ce076108f",
+    "url": "/static/js/main.678be318.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
